649453345e43006601b28798400629a0a7946c5b
joaomteixeira11 <joao.m.teixeira@aitec.pt>
2024-07-16 17:20:42 +0100

    Error handiling

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,16 @@
+using API;
 using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
-
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
